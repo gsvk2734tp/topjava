038a6f27ef6b7648e1cfdd55d490818e4ea50116
gsvk2734tp@gmail.com <B4b732555>
2019-02-10 19:03:24 +0300

    update init and populate DB SQL-scripts

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
+
 
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -9,12 +10,12 @@ CREATE SEQUENCE global_seq START 100000;
 CREATE TABLE users
 (
   id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name             VARCHAR                 NOT NULL,
-  email            VARCHAR                 NOT NULL,
-  password         VARCHAR                 NOT NULL,
-  registered       TIMESTAMP DEFAULT now() NOT NULL,
-  enabled          BOOL DEFAULT TRUE       NOT NULL,
-  calories_per_day INTEGER DEFAULT 2000    NOT NULL
+  name             VARCHAR                           NOT NULL,
+  email            VARCHAR                           NOT NULL,
+  password         VARCHAR                           NOT NULL,
+  registered       TIMESTAMP           DEFAULT now() NOT NULL,
+  enabled          BOOL                DEFAULT TRUE  NOT NULL,
+  calories_per_day INTEGER             DEFAULT 2000  NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
@@ -28,5 +29,11 @@ CREATE TABLE user_roles
 
 CREATE TABLE meals
 (
-
-)
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id     INTEGER   NOT NULL,
+  date_time   TIMESTAMP NOT NULL,
+  description VARCHAR   NOT NULL,
+  calories    INTEGER   NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX date_time_unique_idx ON meals (date_time);
