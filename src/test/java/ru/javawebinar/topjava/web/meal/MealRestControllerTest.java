07d073d5bfbf1d952ad6a1cefad452dfd5af7348
gsvk2734tp@gmail.com <B4b732555>
2019-02-20 23:36:03 +0300

    ADD MealRestController and test with POSTMAN

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealRestControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void createWithLocation() {
+    }
+
+    @Test
+    void getBetween() {
+    }
+}
